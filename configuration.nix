2a278622f9702bf87a0b262b9f026bf3bf6974f6
Łukasz Zając <4561094+ToRRent1812@users.noreply.github.com>
2025-05-14 22:59:39 +0200

    v10

--- configuration.nix
+++ configuration.nix
@@ -150,12 +150,41 @@ in
       enable = true;
       alsa.enable = true;
       alsa.support32Bit = true;
-      pulse.enable = false; # Emulacja Pulseaudio
+      pulse.enable = true; # Emulacja Pulseaudio
+      extraConfig.pipewire."92-low-latency" = { # RT audio
+        "context.properties" = {
+          "default.clock.rate" = 48000;
+          "default.clock.quantum" = 32;
+          "default.clock.min-quantum" = 32;
+          "default.clock.max-quantum" = 32;
+        };
+      };
+      extraConfig.pipewire-pulse."92-low-latency" = {
+        context.modules = [
+        {
+          name = "libpipewire-module-protocol-pulse";
+          args = {
+            pulse.min.req = "32/48000";
+            pulse.default.req = "32/48000";
+            pulse.max.req = "32/48000";
+            pulse.min.quantum = "32/48000";
+            pulse.max.quantum = "32/48000";
+          };
+        }
+      ];
+      stream.properties = {
+        node.latency = "32/48000";
+        resample.quality = 1;
+      };
+    };
+};
+
     };
 
     libinput.enable = false; # Wsparcie touchpadów
   };
   console.keyMap = "pl2"; # Polska klawiatura
+
 
   # Zezwól na audio w priorytecie realtime
   security.rtkit.enable = true;
